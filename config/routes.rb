b679f3b30f2e0cbcaffbd8d829edad65b2c3428b
Juan Vergara <juanvena@unisabana.edu.co>
2019-04-30 19:50:07 +0000

    adds views

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,13 @@
 Rails.application.routes.draw do
-  get 'static_pages/BASICS'
+  get 'static_pages/basics'
 
-  get 'static_pages/RUBY'
-
-  get 'static_pages/GIT'
+  get 'static_pages/ruby'
 
   get 'static_pages/home'
 
-  get 'static_pages/Basics'
+  get 'static_pages/ruby'
 
-  get 'static_pages/Ruby'
+  get 'static_pages/git'
 
-  get 'static_pages/Git'
-
-  root 'application#hello'
+  root 'static_pages#home'
 end
